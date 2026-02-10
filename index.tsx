b8c0ac656df132db47ad94d6bca3f4ad5908be22
imraan447 <imraan447@gmail.com>
2026-02-10 06:38:47 +0200

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Could not find root element");
-const root = createRoot(rootElement);
+const root = createRoot(document.getElementById('root')!);
 root.render(<React.StrictMode><App /></React.StrictMode>);
