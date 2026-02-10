e4430ccc642325f71117d7c4104c2c1569ca6461
imraan447 <imraan447@gmail.com>
2026-02-10 23:16:16 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App'; // Removed .tsx extension
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
