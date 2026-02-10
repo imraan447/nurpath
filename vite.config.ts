602f5e45030079bbd8e79eb7a0dbb122474b1b26
imraan447 <imraan447@gmail.com>
2026-02-10 23:17:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
