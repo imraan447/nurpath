73339d455c464633860b7e09a3b68d67f44dc747
imraan447 <imraan447@gmail.com>
2026-02-10 05:48:09 +0200

    Add files via upload

---
+++ geminiService.ts
@@ -0,0 +1,3 @@
+import { GoogleGenAI, Type } from "@google/genai";
+export async function generateReflections(count: number = 5) { /* ... */ }
+export async function findNearbyPlace(query: string, latitude: number, longitude: number) { /* ... */ }
